6abba523ef60f56f0a81c2e3ce1a1350d0d9666d
sandhya Jagtap <sandhyajagtap05022000@gmail.com>
2024-10-26 11:37:41 +0530

    first test case fail

--- src/main/java/org/example/StringCalculator.java
+++ src/main/java/org/example/StringCalculator.java
@@ -2,5 +2,15 @@ package org.example;
 
 public class StringCalculator {
 
+    public int add(String numbers) {
+        if (numbers.isEmpty()) {
+            return 1;
+        }
 
+        return 0;
+    }
+
+//        public static void main(String[] args) {
+//            System.out.println("Run test cases to verify functionality.");
+//        }
 }
