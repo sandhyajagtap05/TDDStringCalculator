e73742a186f28166cd03df01cbe631dee9378fa8
sandhya Jagtap <sandhyajagtap05022000@gmail.com>
2024-10-26 11:45:20 +0530

    second test case pass

--- src/main/java/org/example/StringCalculator.java
+++ src/main/java/org/example/StringCalculator.java
@@ -7,8 +7,14 @@ public class StringCalculator {
             return 0;
         }
 
+        if (!numbers.contains(",")) { // Step 2: Check for a single number
+            return Integer.parseInt(numbers); // Return the number itself
+        }
+
         return 0;
     }
+
+
 
 //        public static void main(String[] args) {
 //            System.out.println("Run test cases to verify functionality.");
